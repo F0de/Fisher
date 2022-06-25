f3b81ca0a8db19be1fae4e344437355193633a9f
Влад Тимчук <mac.free.lit@gmail.com>
2022-06-25 14:47:51 +0300

    Initial Commit

---
+++ Fisher/FisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FisherApp.swift
+//  Fisher
+//
+//  Created by Влад Тимчук on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
